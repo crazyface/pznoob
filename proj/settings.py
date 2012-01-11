126ed3686c8ba193017e10d652715024c7f89dd2
Alexandr Sorokin <bsorokind@gmail.com>
2012-01-11 19:13:12 +0200

    change settings

--- proj/settings.py
+++ proj/settings.py
@@ -143,11 +143,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.flatpages',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django.contrib.flatpages',
 
     # Installed apps
     'south',
