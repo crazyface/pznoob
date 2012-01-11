b7982ee044ec505b8fc041609b37728a3c663f65
Alexandr Sorokin <bsorokind@gmail.com>
2012-01-11 19:19:09 +0200

    change admin prefix

--- proj/settings.py
+++ proj/settings.py
@@ -79,7 +79,7 @@ UPLOAD_URL = 'uploads/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
